4ed35bfdd94be1d7c58073971aeafa81eef0b5e0
dizsart <darogadiba@gmail.com>
2021-05-10 10:39:58 +0100

    add new methods

--- src/main/java/net/codejava/AppController.java
+++ src/main/java/net/codejava/AppController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
+
 @Controller
 public class AppController {
 
